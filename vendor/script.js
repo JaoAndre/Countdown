13478e3c74fe91ecfb500a11f9d9328043cee386
John <59805284+JaoAndre@users.noreply.github.com>
2021-12-22 14:17:16 -0300

    Update script.js

--- vendor/script.js
+++ vendor/script.js
@@ -1,17 +1,15 @@
-var countDate = new Date("Jan 1, 2022 00:00:00").getTime();
+const countDate = new Date('Jan 1, 2022 00:00:00').getTime();
 
 function countDown() {
-    var now = new Date().getTime();
-    var distance = countDate - now;
+    const now = new Date().getTime();
+    const distance = countDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.querySelector(".countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    document.querySelector('.countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
-if (countDate > new Date().getTime()) {
-    setInterval(countDown, 1000);
-}
+setInterval(countDown, 1000);
